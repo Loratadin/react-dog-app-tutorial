6eeccba0d41b1dc9956cda4fd63313edf170d869
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-09 13:25:01 -0500

    Set up the dependenses and App.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div>Menu</div>
+                <div>Select</div>
+                <div>Breed Image</div>
+            </div>
+        )
+    }
 }
 
 export default App;
