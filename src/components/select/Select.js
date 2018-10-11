ffa493fd6f83356c3e3ebdad1e2b6f7d480fc660
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-11 14:31:24 -0500

    Create Select component

---
+++ src/components/select/Select.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './select.css';
+import '../breed-image/breed-image.css';
+
+const Select = () => {
+    return(
+        <div className="select-container"></div>
+    );
+}
+
+export default Select;
