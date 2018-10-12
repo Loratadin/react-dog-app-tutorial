454c2ff2d3de4dce080e5bb632c88a2c9b13db62
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-12 09:45:43 -0500

    Add toggle menu classes function

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './menu.css';
 
+const toggleMenuClasses = () => {
+    let hamburgerIcon = document.getElementById('burger');
+    hamburgerIcon.classList.toggle('is-active');
+    let mobileMenu = document.getElementById('menu-container');
+    mobileMenu.classList.toggle('opened');
+} 
 const Menu = () => {
     return(
         <div>
-            <div className="burger">
+            <div className="burger" id="burger" onClick={toggleMenuClasses}>
                 <div className="lines">
                     <div className="line"></div>
                     <div className="line"></div>
                     <div className="line"></div>
                 </div>
             </div>
-            <div className="menu-container">
+            <div className="menu-container" id="menu-container">
                 <div className="menu-logo"><img className="menu-logo-img" src={require('../../assets/logo.png')} alt="dog"/></div>
-                <div className="menu-title"></div>
+                <div className="menu-title">Breed Finder</div>
                 <a className="menu-github" href="https://github.com/Loratadin/dog-breed-search" target="_blank">Source Code in Git</a>
                 <a className="menu-twitter" href="https://twitter.com/PAIRProgrammin1?lang=en" target="_blank" >Follow on Twitter</a>
             </div>
