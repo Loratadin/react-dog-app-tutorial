30f243f93c8bb13f25ce6b5ebb87f6dbd775d89d
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-12 09:37:16 -0500

    Import Menu in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import BreedImage from './components/breed-image/BreedImage';
 import Select from './components/select/Select';
+import Menu from './components/menu/Menu';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <div>Menu</div>
+                <Menu/>
                 <Select/>
                 <BreedImage/>
             </div>
