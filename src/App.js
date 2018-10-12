d7acac96c8bb7d984a5080add97618c82e412e28
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-12 10:55:15 -0500

    Clean up the code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import BreedImage from './components/breed-image/BreedImage';
-import Select from './components/select/Select';
 import Menu from './components/menu/Menu';
+import Select from './components/select/Select';
+import BreedImage from './components/breed-image/BreedImage';
 
 class App extends Component {
-    render() {
+    render( ) {
+        console.log(this.state.selectedBreed);
         return (
             <div className="App">
                 <Menu/>
                 <Select/>
                 <BreedImage/>
             </div>
-        )
+        );
     }
 }
 
