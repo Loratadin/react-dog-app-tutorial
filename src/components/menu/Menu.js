1fde54df553816c8e8bcc250aede1cd549c77556
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-12 09:35:38 -0500

    Add styles for Menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -12,7 +12,7 @@ const Menu = () => {
                 </div>
             </div>
             <div className="menu-container">
-                <div className="menu-logo"><img className="menu-logo-img" src={require('../../assets/logo.png')}/></div>
+                <div className="menu-logo"><img className="menu-logo-img" src={require('../../assets/logo.png')} alt="dog"/></div>
                 <div className="menu-title"></div>
                 <a className="menu-github" href="https://github.com/Loratadin/dog-breed-search" target="_blank">Source Code in Git</a>
                 <a className="menu-twitter" href="https://twitter.com/PAIRProgrammin1?lang=en" target="_blank" >Follow on Twitter</a>
